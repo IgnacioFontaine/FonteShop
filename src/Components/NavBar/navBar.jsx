d390fe50a80c9b5d949f611bfd4975e14d2dbe52
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-28 11:57:34 -0300

    navBar en proceso

---
+++ src/Components/NavBar/navBar.jsx
@@ -0,0 +1,19 @@
+import "./navBar.css"
+
+export function NavBar() {
+  return (
+    <section>
+      <header>
+        <h1>FonteShop</h1>
+      </header>
+      <body>
+        <h3>SearchBar</h3>
+      </body>
+      <footer>
+        <but>Me Gusta</but>
+        <but>Carrito</but>
+        <but>SingIn</but>
+      </footer>
+    </section>
+  )
+}
