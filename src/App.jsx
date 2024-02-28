e1cdf2d12da33ceb5f5b460eff22bcf28416fdf8
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-28 11:27:50 -0300

    creando componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css"
+import { ProductCard } from './Components/Product/productCard';
 
 function App() {
 
   return (
     <div>
-      <h1 class="container py-4 px-3 mx-auto bg-blue">FontShop</h1>
+      <h1 className="container py-4 px-3 mx-auto bg-blue">FontShop</h1>
+      <ProductCard></ProductCard>
     </div>
   )
 }
