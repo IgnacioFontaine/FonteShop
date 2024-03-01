e3dae08d26c28a0cf23db514ae35e3bf0d99a45f
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-01 00:29:37 -0300

    componente total funcionando

--- src/Components/total.jsx
+++ src/Components/total.jsx
@@ -1,11 +1,17 @@
 import { Box, Typography, Button } from "@mui/material"
+import store from "../Redux/store";
+
 
 export function Total() {
+  const subscribe_store = store.getState()  
+  const purchease_products = subscribe_store.products.shop_product;
+  const total_price = purchease_products.reduce((total, objeto) => total + objeto.price, 0);
+
     return (
       <Box container sx={{ maxWidth: 400, maxHeight:450, backgroundColor:'#FFA657', color: "black", borderRadius: 2, boxShadow: 6, p:1, mr:2  }}>
             <Typography variant="h4" fontFamily={"fantasy"}>Purchase Summary</Typography>
-            <Typography variant="h5">Items: 2</Typography>
-            <Typography variant="h5">Total: $2.000</Typography>
+        <Typography variant="h5">Items: {purchease_products.length}</Typography>
+        <Typography variant="h5">Total: ${total_price}</Typography>
             <Button variant="contained" sx={{bgcolor:"#FEDDBF", color:"black", ":hover":{bgcolor:'#FFA657', color:"white"}}}>Continue</Button>
           </Box>
     )
