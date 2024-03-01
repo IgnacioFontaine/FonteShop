f69539f25e0862bea6342b555b07110fec9df0ee
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-01 12:52:17 -0300

    singun view

---
+++ src/assets/Views/singUnView.jsx
@@ -0,0 +1,10 @@
+import SignUp from "../../Components/singUp";
+import { Box } from "@mui/material";
+
+export default function SingInView() {
+  return (
+    <Box sx={{width:1360}}>
+      <SignUp/>
+    </Box>
+  )
+}
