b86d82406d87c2ba7015d85d163a7be05eb23f96
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-01 09:46:41 -0300

    funcional like view

--- src/assets/Views/likeView.jsx
+++ src/assets/Views/likeView.jsx
@@ -2,12 +2,15 @@ import { Grid, Typography} from "@mui/material";
 import products from "../Data/dataProducts";
 import ProductCard from "../../Components/Product/productCard";
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import store from "../../Redux/store";
 
 export default function LikedView() {
+  const subscribe_store = store.getState()
+  const favorite_products = subscribe_store.products.shop_product;
 
   function LikeProducts() {
     return (<>
-      {products.map((item)=> (
+      {favorite_products.map((item)=> (
         <Grid item xs={12} sm={8} md={6} lg={4} key={item.id}>
             <ProductCard key={item.id} product={item} />
           </Grid>
@@ -24,8 +27,10 @@ export default function LikedView() {
           <Typography variant="h2" fontFamily={"fantasy"}>favorites</Typography>
           <FavoriteBorderIcon fontSize="large"/>
         </Grid>
-        <Grid item xs={12} sm={8} md={9} container spacing={3} sx={{ml:7}} >
-          <LikeProducts  />
+        <Grid item xs={12} sm={8} md={9} container spacing={3} sx={{ ml: 7 }} >
+          {favorite_products.length >0 ?
+          <LikeProducts  />:<Typography variant="h2"fontFamily={"fantasy"} sx={{height:550, width:550, textAlign:"center", justifyContent:"center"}}>Not Favorites yet</Typography>}
+          
         </Grid>
         <Grid item xs={12}>
           <Typography variant="h4" fontFamily={"fantasy"}>They can be Yours!</Typography>
