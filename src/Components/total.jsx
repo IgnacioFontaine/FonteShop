83beb55ed866fa6d2be52302b995cf11773e742e
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-04 14:15:05 -0300

    implementando MercadoPago

--- src/Components/total.jsx
+++ src/Components/total.jsx
@@ -1,6 +1,10 @@
-import { Box, Typography, Button } from "@mui/material"
 import store from "../Redux/store";
+import { Box, Typography, Button } from "@mui/material"
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { initMercadoPago, Wallet } from '@mercadopago/sdk-react'
+
+
 
 
 export function Total() {
@@ -9,6 +13,25 @@ export function Total() {
   const subscribe_store = store.getState()  
   const purchease_products = subscribe_store.products.shop_product;
   const total_price = purchease_products.reduce((total, objeto) => total + objeto.price, 0);
+
+  //MercadoPago
+  const [preferenceId, setPreferenceId] = useState(null);
+  initMercadoPago('YOUR_PUBLIC_KEY');
+
+  const createPreference = async () => {
+    try {
+      const response = await axios.post("http://localhost:8080/create_preference", {
+        description: "FonteShop Purchease",
+        price: total_price,
+        quantiti:1,
+      })
+
+      const { id } = response.data;
+      return id;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
     return (
       <Box container sx={{ maxWidth: 400, maxHeight:450, backgroundColor:'#FFA657', color: "black", borderRadius: 2, boxShadow: 6, p:1, mr:2  }}>
