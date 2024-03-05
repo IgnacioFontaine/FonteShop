239d21629bf7cbb80b0083205874e274760231ad
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-05 11:38:18 -0300

    cambio products

--- client/src/Components/products.jsx
+++ client/src/Components/products.jsx
@@ -4,12 +4,12 @@ import products from "../assets/Data/dataProducts";
 
 export function Products() {
   return (
-    <Grid container spacing={{ xs: 1, md: 1 }} columns={{ xs: 3, sm: 6, md: 12 }}>
-        {products.map((product)=> (
-          <Grid item xs={3} sm={4} md={4} key={product.id}>
-            <ProductCard key={product.id} product={product} />
-          </Grid>
-        ))}
+    <Grid container spacing={1}>
+      {products.map((product) => (
+        <Grid item xs={12} sm={6} md={4} key={product.id}>
+          <ProductCard key={product.id} product={product} />
+        </Grid>
+      ))}
     </Grid>
-  )
+  );
 }
