2eacb44fb0a4c2693301e14e71c04ead7d912812
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-12 17:06:03 -0300

    cambio en home

--- client/src/Components/Offers/offers.jsx
+++ client/src/Components/Offers/offers.jsx
@@ -1,17 +1,21 @@
 import ProductCard from "../Product/productCard";
-import { Grid, Typography, Box } from "@mui/material";
+import { Typography, Box } from "@mui/material";
 import products from "../../assets/Data/dataProducts";
 
 // Ordenamos los productos por precio de menor a mayor
-const products_in_order = products.sort((a, b) => a.price - b.price);
+const products_in_order = products;
+
+const order = products_in_order.sort((a, b) => a.price - b.price);
+
+
 
 // Tomamos los primeros tres productos
-const inOffer = products_in_order.slice(0, 3);
+const inOffer = order.slice(0, 3);
 
 
 export function Offers() {
   return (
-    <Box sx={{display:"flex", flexDirection:"column", textAlign:"center", mt:3, mb:3, backgroundColor:"white", borderRadius:3,mr:12}}>
+    <Box xs={12} sm={6} md={4} sx={{display:"flex", flexDirection:"column", textAlign:"center", mt:3, mb:3, backgroundColor:"white", borderRadius:3,mr:13}}>
       <Box>
         <Typography variant="h3" color={"#FFA657"} fontFamily={"fantasy"}>Offers</Typography>
       </Box>
--- client/src/Components/products.jsx
+++ client/src/Components/products.jsx
@@ -3,9 +3,10 @@ import { Grid } from "@mui/material";
 import products from "../assets/Data/dataProducts";
 
 export function Products() {
+  const products_original = products.sort((a, b) => b.price - a.price);
   return (
     <Grid container spacing={1}>
-      {products.map((product) => (
+      {products_original.map((product) => (
         <Grid item xs={12} sm={6} md={4} key={product.id}>
           <ProductCard key={product.id} product={product} />
         </Grid>
