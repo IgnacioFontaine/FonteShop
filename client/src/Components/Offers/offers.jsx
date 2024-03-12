090da23005d2ac5fb6811e5dc1ddd137bfab8c1b
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-12 16:10:37 -0300

    offers listo

--- client/src/Components/Offers/offers.jsx
+++ client/src/Components/Offers/offers.jsx
@@ -2,14 +2,18 @@ import ProductCard from "./Product/productCard";
 import { Grid, Typography } from "@mui/material";
 import products from "../assets/Data/dataProducts";
 
+// Ordenamos los productos por precio de menor a mayor
+const products_in_order = products.sort((a, b) => a.price - b.price);
 
+// Tomamos los primeros tres productos
+const inOffer = products_in_order.slice(0, 3);
 
 
 export function Offers() {
   return (
     <Grid container spacing={1} sx={{backgroundColor:"white", borderRadius:4}}>
       <Typography variant="h3" color={"text.primary"}>Offers</Typography>
-      {products.map((product) => (
+      {inOffer.map((product) => (
         <Grid item xs={12} sm={6} md={4} key={product.id}>
           <ProductCard key={product.id} product={product} />
         </Grid>
