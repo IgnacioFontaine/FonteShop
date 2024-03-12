a48df4593b60bf561d60b1293f7c4da4617bb3bd
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-12 16:31:49 -0300

    ofertas casi listo

--- client/src/Components/Offers/offers.jsx
+++ client/src/Components/Offers/offers.jsx
@@ -1,6 +1,6 @@
-import ProductCard from "./Product/productCard";
-import { Grid, Typography } from "@mui/material";
-import products from "../assets/Data/dataProducts";
+import ProductCard from "../Product/productCard";
+import { Grid, Typography, Box } from "@mui/material";
+import products from "../../assets/Data/dataProducts";
 
 // Ordenamos los productos por precio de menor a mayor
 const products_in_order = products.sort((a, b) => a.price - b.price);
@@ -11,13 +11,15 @@ const inOffer = products_in_order.slice(0, 3);
 
 export function Offers() {
   return (
-    <Grid container spacing={1} sx={{backgroundColor:"white", borderRadius:4}}>
-      <Typography variant="h3" color={"text.primary"}>Offers</Typography>
-      {inOffer.map((product) => (
-        <Grid item xs={12} sm={6} md={4} key={product.id}>
+    <Box sx={{display:"flex", flexDirection:"column", textAlign:"center", mt:3, mb:3, backgroundColor:"white", borderRadius:3,mr:12}}>
+      <Box>
+        <Typography variant="h3" color={"#FFA657"} fontFamily={"fantasy"}>Offers</Typography>
+      </Box>
+      <Box sx={{display:"flex", justifyContent:"space-around", mt:3, mb:3, gap:12}}>
+        {inOffer.map((product) => (
           <ProductCard key={product.id} product={product} />
-        </Grid>
       ))}
-    </Grid>
+      </Box>
+    </Box>
   );
 }
--- client/src/assets/Views/home.jsx
+++ client/src/assets/Views/home.jsx
@@ -1,9 +1,11 @@
 import { Box } from "@mui/material";
 import { Products } from "../../Components/products";
+import { Offers } from "../../Components/Offers/offers";
 
 export default function Home() {
   return (
-    <Box sx={{ mt: 1.5, ml:5}}>
+    <Box sx={{ mt: 1.5, ml: 5 }}>
+      <Offers />
       <Products />
     </Box>
   )
