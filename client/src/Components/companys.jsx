9f5b8c2e297ae590b5a8d7792ac8bc069570ce95
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-13 13:03:41 -0300

    nuevo componente companys

---
+++ client/src/Components/companys.jsx
@@ -0,0 +1,9 @@
+import { Typography, Box } from "@mui/material";
+
+export function Companys() {
+  return (
+    <Box xs={12} sm={6} md={4}>
+      <Typography>Acá van las companías de los prod</Typography>
+    </Box>
+  );
+}
