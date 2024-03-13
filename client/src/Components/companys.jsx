b41a40ff30cc22611f7b3fa4a92346a5931b060b
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-13 13:39:54 -0300

    set mostrado en pantalla

--- client/src/Components/companys.jsx
+++ client/src/Components/companys.jsx
@@ -4,14 +4,18 @@ import products from "../assets/Data/dataProducts";
 const uniqueBrandsSet = new Set(products.map(item => item.brand));
 const uniqueBrandsArray = Array.from(uniqueBrandsSet);
 
-console.log(uniqueBrandsArray);
+console.log(uniqueBrandsSet);
 
 
 export function Companys() {
   return (
     <Box xs={12} sm={6} md={4}>
       <Typography variant="h2">Brands</Typography>
-      <Typography variant="body2">{uniqueBrandsArray}</Typography>
+      <Box sx={{display:"flex", p:2, textAlign:"center"}} >
+        {Array.from(uniqueBrandsSet).map((item, index) => (
+          <Typography key={index} sx={{m:2}}>{item}</Typography>
+        ))}
+      </Box>
     </Box>
   );
 }
