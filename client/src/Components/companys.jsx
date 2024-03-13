ebad54482f49700a6592c9d210049e6ed7b65b48
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-13 13:58:02 -0300

    cambio en company

--- client/src/Components/companys.jsx
+++ client/src/Components/companys.jsx
@@ -1,21 +1,48 @@
-import { Typography, Box } from "@mui/material";
+import React, { useState, useEffect } from 'react';
+import { Typography, Box } from '@mui/material';
 import products from "../assets/Data/dataProducts";
 
 const uniqueBrandsSet = new Set(products.map(item => item.brand));
 // const uniqueBrandsArray = Array.from(uniqueBrandsSet);
 
-console.log(uniqueBrandsSet);
+const Companys = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
 
+  // Lógica para desplazamiento infinito
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setScrollPosition(prevPosition => prevPosition + 1);
+    }, 1000); // Ajusta el intervalo según tus necesidades
 
-export function Companys() {
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <Box xs={12} sm={6} md={4}>
       <Typography variant="h2">Brands</Typography>
-      <Box sx={{display:"flex", p:2, textAlign:"center"}} >
-        {Array.from(uniqueBrandsSet).map((item, index) => (
-          <Typography key={index} sx={{m:2}}>{item}</Typography>
+      <div
+        style={{
+          display: 'flex',
+          overflow: 'hidden',
+          whiteSpace: 'nowrap',
+        }}
+      >
+        {uniqueBrandsSet.map((brand, index) => (
+          <Typography
+            key={index}
+            sx={{
+              m: 2,
+              transform: `translateX(-${scrollPosition * 100}px)`, // Ajusta la cantidad de desplazamiento
+              transition: 'transform 0.5s ease', // Añade una transición suave
+            }}
+          >
+            {brand}
+          </Typography>
         ))}
-      </Box>
+      </div>
     </Box>
   );
-}
+};
+
+export default Companys;
+
